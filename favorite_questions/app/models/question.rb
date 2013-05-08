53614851a19b00e15616596ff3f88d2695c5fc0a
Meris Williams <meris@Meriss-MacBook-Air.local>
2013-05-07 23:00:17 -0700

    Fav Qs app of the day

---
+++ favorite_questions/app/models/question.rb
@@ -0,0 +1,4 @@
+class Question < ActiveRecord::Base
+  validates :question, :presence => true
+  attr_accessible :answer, :question
+end
---
+++ favorite_questions/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FavoriteQuestions::Application.config.secret_token = '7bff58e9249b70abc0981e163fa3963879bb3dabc8cad2da33c2c3c82f07396b7617d5d5f1b515bf13a7166a8fd116220ca9bb314d39151f9c5d94db60c664be'
